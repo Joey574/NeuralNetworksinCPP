34f60bd8c8c42bb58507c978e2bba0b1c2f9bb39
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2024-03-10 21:33:19 -0700

    small work with actual network

--- LogisticRegressionStandalone/LogisticRegressionStandalone.cpp
+++ LogisticRegressionStandalone/LogisticRegressionStandalone.cpp
@@ -4,11 +4,28 @@
 
 using namespace std;
 
+// Inputs
+Matrix input;
+Matrix batch;
+
+// Neural Network Matrices
+vector<Matrix> weights;
+Matrix biases;
+
+vector<Matrix> activation;
+vector<Matrix> aTotal;
+
+vector<Matrix> dTotal;
+vector<Matrix> dWeights;
+Matrix dBiases;
+
+// Prototype
+
+Matrix ReLU(Matrix total);
+
 int main()
 {
-	Matrix t = Matrix(10, 100, -0.5f, 0.5f);
 
-	cout << t.ColumnSums()[0];
 }
 
 void InitializeNetwork() {
@@ -20,7 +37,10 @@ void TrainNetwork() {
 }
 
 void ForwardPropogation() {
-
+	for (int i = 0; i < aTotal.size(); i++) {
+		aTotal[i] = (weights[i] * (i == 0 ? batch : activation[i - 1])) + biases[i];
+		activation[i] = ReLU(aTotal[i]);
+	}
 }
 
 void BackwardPropogation() {
@@ -29,4 +49,15 @@ void BackwardPropogation() {
 
 void UpdateNetwork() {
 
+}
+
+Matrix ReLU(Matrix total) {
+	Matrix a = total;
+
+	for (int r = 0; r < total.RowCount; r++) {
+		for (int c = 0; c < total.ColumnCount; c++) {
+			a[r][c] = total[r][c] < 0.0f ? 0.0f : total[r][c];
+		}
+	}
+	return a;
 }
