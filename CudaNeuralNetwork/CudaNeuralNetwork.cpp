cc94002141739cfc8786faad9e895b9d714cc8e0
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2024-04-15 19:50:50 -0700

    stuff

---
+++ CudaNeuralNetwork/CudaNeuralNetwork.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+__global__ void train_netowrk() {
+
+}
+
+__device__ void forward_propogation(float* weights, float* biases, float* input) {
+
+}
+
+__device__ void backward_propogation() {
+
+}
+
+__device__ void leakyrelu(float* matrix, float scalar) {
+
+}
+
+__device__ void leakyrelu_derivative(float* matrix, float scalar) {
+
+}
+
+
+int main()
+{
+
+}
